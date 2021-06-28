b9dce10d59d0f5e34f3bd6c869248e637a6c68d0
Bhanu Pratap Singh <bp.mnnit@gmail.com>
2021-06-28 23:22:05 +0530

    Added the ref functionality to close the dropdown whenever user click anywhere outside.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,22 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
   
+  useEffect(() => {
+    document.body.addEventListener('click', (event) => {
+      if(ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+      // Remember manual js event listeners are called first
+      // And, then all the event listeners we create in react are called
+    }, { capture: true });
+  },[]);
+
   const renderedOptions = options.map((option) => {
     if(option.value === selected.value) {
       return null;
     }
 
     return (
-      <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}>
         {option.label}
       </div>
     );
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
